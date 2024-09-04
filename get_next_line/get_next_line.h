5b0d253b50ed86624eed1c694a10f38026111f35
Guilherme De Andrade Sales <gude-and@c2r6s13.42lisboa.com>
2024-09-04 08:19:38 +0100

    First commit

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gude-and <gude-and@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 07:24:43 by gude-and          #+#    #+#             */
+/*   Updated: 2024/09/04 08:02:15 by gude-and         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*str_buffer;
+	struct s_list	*next;
+}					t_list;
+
